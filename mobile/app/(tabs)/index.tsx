b45587bae3b9a8a2f2543bca2759f6e27e9be9bb
José Núñez <79015124+josiext@users.noreply.github.com>
2024-12-12 12:28:52 -0300

    updates

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -5,18 +5,6 @@ import * as Linking from "expo-linking";
 const WEB_URL = "http://10.0.2.2:5173";
 
 export default function TabOneScreen() {
-  const url = Linking.useURL();
-
-  if (url) {
-    const { hostname, path, queryParams } = Linking.parse(url);
-
-    console.log(
-      `Linked to app with hostname: ${hostname}, path: ${path} and data: ${JSON.stringify(
-        queryParams
-      )}`
-    );
-  }
-
   return (
     <View style={styles.container}>
       <Button title="Open Web" onPress={() => Linking.openURL(WEB_URL)} />
@@ -29,14 +17,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
   },
 });
